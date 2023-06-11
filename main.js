f7fc25106dda870ee5b9895a20edf58d7b938470
Rian Ruggles <rs.ruggles@gmail.com>
2023-06-11 10:07:24 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const {app, BrowserWindow, ipcMain} = require('electron')
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 768,
+        height: 432,
+        icon: 'icon.ico',
+        frame: false,
+        webPreferences:{
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    })
+
+
+
+    mainWindow.loadFile('index.html')
+
+    ipcMain.on('msg', (e, data)=>{
+        console.warn(data)
+    })
+    ipcMain.on('close', () => {
+        app.quit()
+    })
+    ipcMain.on('fullScreen', () => {
+        mainWindow.setFullScreen(true);
+    })
+    ipcMain.on('windowScreen', () => {
+        mainWindow.setFullScreen(false);
+    })
+    ipcMain.on('minimize', () => {
+        mainWindow.minimize();
+    })
+
+
+    mainWindow.setAspectRatio(16/9);
+
+}
+
+
+
+app.whenReady().then(createWindow)
+
